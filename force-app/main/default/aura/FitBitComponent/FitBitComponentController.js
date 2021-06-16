08e535173a03f7afe43ccc0d42773fa953bc1391
Thomas Philippou <thomas.philippou@uppersigma.com>
2021-06-16 18:45:12 +0100

    Changes to FitBit Classes

--- force-app/main/default/aura/FitBitComponent/FitBitComponentController.js
+++ force-app/main/default/aura/FitBitComponent/FitBitComponentController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    doInit: function (component, event, helper) {
+        component.set("v.showSpinner", true);
+        helper.getFitbitData(component, helper);
+        helper.getTodaySummary(component, helper);
+        helper.getFriendsDetails(component, helper);
+        helper.getmyBadges(component, helper);
+        component.set("v.showSpinner", false);
     }
-})
+});
--- force-app/main/default/aura/FitBitComponent/FitBitComponentHelper.js
+++ force-app/main/default/aura/FitBitComponent/FitBitComponentHelper.js
@@ -1,5 +1,54 @@
 ({
-    helperMethod : function() {
-
+    getFitbitData: function (component) {
+        var action = component.get("c.getUserDetails");
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var parseJson = JSON.parse(response.getReturnValue());
+                component.set("v.user", parseJson.user);
+            } else if (state === "ERROR") {
+                console.log("Error");
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    getTodaySummary: function (component, helper) {
+        var summaryData = component.get("c.getTodaySummary");
+        summaryData.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var parseJson = JSON.parse(response.getReturnValue());
+                component.set("v.todayData", parseJson);
+            } else if (state === "ERROR") {
+                console.log("Error");
+            }
+        });
+        $A.enqueueAction(summaryData);
+    },
+    getFriendsDetails: function (component, helper) {
+        var friendsList = component.get("c.getFriendsList");
+        friendsList.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var parseJson = JSON.parse(response.getReturnValue());
+                component.set("v.friends", parseJson.friends);
+            } else if (state === "ERROR") {
+                console.log("Error");
+            }
+        });
+        $A.enqueueAction(friendsList);
+    },
+    getmyBadges: function (component, helper) {
+        var badges = component.get("c.getMybadges");
+        badges.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var parseJson = JSON.parse(response.getReturnValue());
+                component.set("v.badges", parseJson.badges);
+            } else if (state === "ERROR") {
+                console.log("Error");
+            }
+        });
+        $A.enqueueAction(badges);
     }
-})
+});
