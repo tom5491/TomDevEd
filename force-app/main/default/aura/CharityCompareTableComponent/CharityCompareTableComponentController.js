740fb81dec24fa910816c18823c9e30f0eaba5ae
Thomas Philippou <thomas.philippou@uppersigma.com>
2021-09-01 20:58:47 +0100

    With Charity Commission cmp

---
+++ force-app/main/default/aura/CharityCompareTableComponent/CharityCompareTableComponentController.js
@@ -0,0 +1,19 @@
+/**
+ * @description       : 
+ * @author            : Tom Philippou
+ * @group             : 
+ * @last modified on  : 27-08-2021
+ * @last modified by  : Tom Philippou 
+ * Modifications Log
+ * Ver   Date         Author          Modification
+ * 1.0   27-08-2021   Tom Philippou   Initial Version
+**/
+({
+    radioChange: function (component, event, helper) {
+        var eventSource = event.getSource();
+        var inputName = eventSource.get("Name");
+        if(){
+            
+        }
+    }
+});
