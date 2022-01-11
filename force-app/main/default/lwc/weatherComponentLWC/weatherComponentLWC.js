8cdb1583595a6d9497a7b2b70a56a08705404882
Thomas Philippou <t.philippou@forcology.com>
2022-01-11 17:04:24 +0000

    weather api updates

--- force-app/main/default/lwc/weatherComponentLWC/weatherComponentLWC.js
+++ force-app/main/default/lwc/weatherComponentLWC/weatherComponentLWC.js
@@ -1,18 +1,42 @@
 import { LightningElement, wire } from 'lwc';
 import weatherCredentials from '@salesforce/apex/GetIntegrationCredentials.getCredentials';
+import FetchWrapper from 'c/utilities';
 
 export default class WeatherComponentLWC extends LightningElement {
     integrationObject;
+    fetchWrapper;
+    endpoint = "weather";
+    city = "London";
 
     @wire(weatherCredentials, {"integrationName": "Open Weather"})
     wiredIntegration({ error, data }) {
         if (data) {
             console.log('data:', data);
             this.integrationObject = data;
-            this.onTodayDate();
-            this.endpoint = "planetary/apod";
+            this.instantiateFetchWrapper();
         } else if (error) {
             console.log('Something went wrong:', error);
         }
     }
+
+    instantiateFetchWrapper(){
+        this.fetchWrapper = new FetchWrapper({ baseURL:this.integrationObject.Base_Url__c})
+        .setUrlParameters({
+            "q": "London,uk", 
+            "appId": this.integrationObject.Authentication_Token__c,
+            "units": "metric"
+        });
+        this.handleFetch();
+    }
+
+    handleFetch(){
+        this.fetchWrapper.get(this.endpoint, {})
+        .then(data => {
+            console.log("Weather Data: " + JSON.stringify(data));
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+        });
+    }
+
 }
