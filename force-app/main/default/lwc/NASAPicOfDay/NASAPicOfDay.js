a7f4d678d06bd5856d236368327822d65b2f6a38
Tom <tom5491@googlemail.com>
2022-01-15 16:20:51 +0000

    weather update

--- force-app/main/default/lwc/NASAPicOfDay/NASAPicOfDay.js
+++ force-app/main/default/lwc/NASAPicOfDay/NASAPicOfDay.js
@@ -1,93 +1,94 @@
-import { LightningElement, wire } from 'lwc';
-import getNasaApiKey from '@salesforce/apex/NASAPicOfDayController.getNasaApiKey';
+import { LightningElement, wire } from "lwc";
+import getNasaApiKey from "@salesforce/apex/NASAPicOfDayController.getNasaApiKey";
 import fetchWrapper from "./fetch-wrapper.js";
 
 export default class nasaPicOfDay extends LightningElement {
-    endpoint = "planetary/apod";
-    urlParameters = {};
+  endpoint = "planetary/apod";
+  urlParameters = {};
 
-    nasaApiKey;
-    fetchUrl;
-    nasaPicObj;
-    isImage;
-    isToday;
-    showCopyright;
-    currentDate;
-    nasaFetchWrapper;
-    
-    @wire(getNasaApiKey)
-    wiredIntegration({ error, data }) {
-        if (data) {
-            this.nasaApiKey = data;
-            this.onTodayDate();
-            this.endpoint = "planetary/apod";
-        } else if (error) {
-            console.log('Something went wrong:', error);
-        }
+  nasaApiKey;
+  fetchUrl;
+  nasaPicObj;
+  isImage;
+  isToday;
+  showCopyright;
+  currentDate;
+  nasaFetchWrapper;
+
+  @wire(getNasaApiKey)
+  wiredIntegration({ error, data }) {
+    if (data) {
+      this.nasaApiKey = data;
+      this.onTodayDate();
+      this.endpoint = "planetary/apod";
+    } else if (error) {
+      console.log("Something went wrong:", error);
     }
+  }
 
-    onTodayDate(){        
-        this.urlParameters = {
-            "api_key": this.nasaApiKey.Authentication_Token__c
-        }
+  onTodayDate() {
+    this.urlParameters = {
+      api_key: this.nasaApiKey.Authentication_Token__c
+    };
 
-        this.handleFetch();
+    this.handleFetch();
+  }
+
+  onDateChange(event) {
+    const buttonName = event.target.name;
+    const nasaApiKey = this.nasaApiKey;
+    // console.log("buttonName: " + buttonName);
+    if (buttonName === "backDate") {
+      this.currentDate = this.currentDate.setDate(this.currentDate.getDate() - 1);
+    } else {
+      this.currentDate = this.currentDate.setDate(this.currentDate.getDate() + 1);
     }
+    this.urlParameters = {
+      api_key: nasaApiKey.Authentication_Token__c,
+      date: new Date(this.currentDate).toISOString().substring(0, 10)
+    };
+    this.handleFetch();
+  }
 
-    onDateChange(event){
-        const buttonName = event.target.name;
-        const nasaApiKey = this.nasaApiKey;
-        console.log("buttonName: " + buttonName);
-        if(buttonName === 'backDate'){
-            this.currentDate = this.currentDate.setDate(this.currentDate.getDate() - 1);
-        } else {
-            this.currentDate = this.currentDate.setDate(this.currentDate.getDate() + 1);
-        }
-        this.urlParameters = {
-            "api_key": nasaApiKey.Authentication_Token__c,
-            "date": new Date(this.currentDate).toISOString().substring(0, 10)
-        }
-        this.handleFetch();
-    }
+  onRandomPic() {
+    const nasaApiKey = this.nasaApiKey;
+    this.urlParameters = {
+      api_key: nasaApiKey.Authentication_Token__c,
+      count: 1
+    };
 
-    onRandomPic(){
-        const nasaApiKey = this.nasaApiKey;
-        this.urlParameters = {
-            "api_key": nasaApiKey.Authentication_Token__c,
-            "count": 1
-        }
-        
-        this.handleFetch();
-    }
+    this.handleFetch();
+  }
 
-    isTodayCheck(){
-        console.log("todays date: " + new Date());
-        console.log("current date: " + new Date(this.currentDate));
-        if(new Date().toISOString().substring(0,10) == new Date(this.currentDate).toISOString().substring(0,10)){
-            this.isToday = true; 
-        } else {
-            this.isToday = false;
-        }
+  isTodayCheck() {
+    // console.log("todays date: " + new Date());
+    // console.log("current date: " + new Date(this.currentDate));
+    if (new Date().toISOString().substring(0, 10) == new Date(this.currentDate).toISOString().substring(0, 10)) {
+      this.isToday = true;
+    } else {
+      this.isToday = false;
     }
+  }
 
-    handleFetch(){
-        const nasaFetchWrapper = new fetchWrapper({ baseURL: this.nasaApiKey.Base_Url__c }).setUrlParameters(this.urlParameters);
-        console.log("handleFetch endpoint: " + this.endpoint);
-        nasaFetchWrapper.get(this.endpoint, {})
-        .then(data => { 
-            console.log(data);
-            if(Array.isArray(data)){
-                data = data[0];
-            }
-            this.currentDate = new Date(data.date);
-            this.isTodayCheck();
-            this.showCopyright = data.copyright !== undefined && data.copyright !== null;
-            if(data.media_type === 'image'){
-                this.isImage = true;
-            } else {
-                this.isImage = false;
-            }
-            this.nasaPicObj = data;
-        });
-    }
+  handleFetch() {
+    const nasaFetchWrapper = new fetchWrapper({ baseURL: this.nasaApiKey.Base_Url__c }).setUrlParameters(
+      this.urlParameters
+    );
+    // console.log("handleFetch endpoint: " + this.endpoint);
+    nasaFetchWrapper.get(this.endpoint, {}).then((data) => {
+      // console.log(data);
+      if (Array.isArray(data)) {
+        data = data[0];
+      }
+      this.currentDate = new Date(data.date);
+      this.isTodayCheck();
+      this.showCopyright = data.copyright !== undefined && data.copyright !== null;
+      if (data.media_type === "image") {
+        this.isImage = true;
+      } else {
+        this.isImage = false;
+      }
+      this.nasaPicObj = data;
+    });
+  }
 }
--- force-app/main/default/lwc/weatherComponentLWC/weatherComponentLWC.js
+++ force-app/main/default/lwc/weatherComponentLWC/weatherComponentLWC.js
@@ -1,110 +1,219 @@
-import { LightningElement, track, wire } from 'lwc';
-import weatherCredentials from '@salesforce/apex/GetIntegrationCredentials.getCredentials';
-import {FetchWrapper} from 'c/utilities';
-import weatherIcons from '@salesforce/resourceUrl/WeatherIcons';
+import { LightningElement, track, wire } from "lwc";
+import weatherCredentials from "@salesforce/apex/GetIntegrationCredentials.getCredentials";
+import { FetchWrapper } from "c/utilities";
+import weatherIcons from "@salesforce/resourceUrl/WeatherIcons";
+import { loadScript } from "lightning/platformResourceLoader";
+import chartminjs from "@salesforce/resourceUrl/chartminjs";
+import chartJsAdapterDate from "@salesforce/resourceUrl/chartJsAdapterDate";
 
 export default class WeatherComponentLWC extends LightningElement {
-    integrationObject;
-    fetchWrapper;
-    endpoint = "weather";
-    city = "London,uk";
-    selectedWeatherIconMap = {};
-    weatherData;
-    weatherIconPath = weatherIcons + '/WeatherIcons/';
-    dailyData;
-    weatherIconMap = {
-                "Thunderstorm": "038-storm-3.png",
-                "Drizzle": "008-rainy",
-                "Rain": "028-rainy-1",
-                "Snow": "019-snowy.png",
-                "Clear": "005-sun.png",
-                "Clouds": "007-cloud.png",
-                "Atmosphere": "036-foog.png"
-            };
+  chartJsInitialised = false;
+  dataInitialised = false;
+  integrationObject;
+  fetchWrapper;
+  endpoint = "weather";
+  city = "London,uk";
+  selectedWeatherIconMap = {};
+  weatherData;
+  weatherIconPath = weatherIcons + "/WeatherIcons/";
+  hourlyData = [];
+  dailyData;
+  chart;
 
-    @wire(weatherCredentials, {"integrationName": "Open Weather"})
-    wiredIntegration({ error, data }) {
-        if (data) {
-            console.log('data:', data);
-            this.integrationObject = data;
-            this.instantiateWeatherFetchWrapper();
-        } else if (error) {
-            console.log('Something went wrong:', error);
-        }
+  weatherIconMap = {
+    Thunderstorm: "038-storm-3.png",
+    Drizzle: "008-rainy.png",
+    Rain: "028-rainy-1.png",
+    Snow: "019-snowy.png",
+    Clear: "005-sun.png",
+    Clouds: "007-cloud.png",
+    Atmosphere: "036-foog.png"
+  };
+
+  @wire(weatherCredentials, { integrationName: "Open Weather" })
+  wiredIntegration({ error, data }) {
+    if (data) {
+      //   console.log("data:", data);
+      this.integrationObject = data;
+      this.instantiateWeatherFetchWrapper();
+    } else if (error) {
+      console.log("Something went wrong:", error);
     }
+  }
 
-    getWeatherIcon(condition){
-        if(!condition in this.weatherIconMap){
-            return this.weatherIconMap.Atmosphere;
+  getWeatherIcon(condition) {
+    if (!(condition in this.weatherIconMap)) {
+      return this.weatherIconMap.Atmosphere;
+    }
+    return this.weatherIconMap[condition];
+  }
+
+  instantiateWeatherFetchWrapper() {
+    this.fetchWrapper = new FetchWrapper({ baseURL: this.integrationObject.Base_Url__c }).setUrlParameters({
+      q: this.city,
+      appId: this.integrationObject.Authentication_Token__c,
+      units: "metric"
+    });
+    this.handleFetch();
+  }
+
+  instantiateOneCallFetchWrapper(lon, lat) {
+    this.fetchWrapper = new FetchWrapper({ baseURL: this.integrationObject.Base_Url__c }).setUrlParameters({
+      lon: lon,
+      lat: lat,
+      appId: this.integrationObject.Authentication_Token__c,
+      exclude: "minutely",
+      units: "metric"
+    });
+    this.endpoint = "onecall";
+    this.handleFetch();
+  }
+
+  handleFetch() {
+    this.fetchWrapper
+      .get(this.endpoint, {})
+      .then((data) => {
+        if (this.endpoint === "weather") {
+          data.weather = data.weather[0];
+          data.weather.icon = this.weatherIconPath + this.getWeatherIcon(data.weather.main);
+          data.dt = String(data.dt).padEnd(13, "0");
+          this.weatherData = data;
+          //   console.log("Weather Data: " + JSON.stringify(data));
+          this.instantiateOneCallFetchWrapper(data.coord.lon, data.coord.lat);
         } else {
-            return this.weatherIconMap[condition];
+          data.daily.splice(0, 1);
+          data.daily.splice(5);
+          data.daily.forEach((day) => {
+            day.weather = day.weather[0];
+            day.weather.icon = this.weatherIconPath + this.getWeatherIcon(day.weather.main);
+            day.dt = String(day.dt).padEnd(13, "0");
+            day.sunrise = String(day.sunrise).padEnd(13, "0");
+            day.sunset = String(day.sunset).padEnd(13, "0");
+            day.moonrise = String(day.moonrise).padEnd(13, "0");
+            day.moonset = String(day.moonset).padEnd(13, "0");
+          });
+          data.hourly = data.hourly.filter(
+            (hr) => new Date(Number.parseInt(String(hr.dt).padEnd(13, "0"), 10)).getHours() % 3 === 0
+          );
+          data.hourly.forEach((hr) => {
+            hr.weather = hr.weather[0];
+            hr.weather.icon = this.weatherIconPath + this.getWeatherIcon(hr.weather.main);
+            hr.dt = String(hr.dt).padEnd(13, "0");
+            hr.sunrise = String(hr.sunrise).padEnd(13, "0");
+            hr.sunset = String(hr.sunset).padEnd(13, "0");
+            hr.moonrise = String(hr.moonrise).padEnd(13, "0");
+            hr.moonset = String(hr.moonset).padEnd(13, "0");
+            const hrObject = { y: Math.round(hr.temp), x: Number.parseInt(hr.dt, 10) };
+            this.hourlyData.push(hrObject);
+          });
+
+          this.dailyData = data.daily;
+
+          //   console.log("hourlyData : " + JSON.stringify(data.hourly));
+          //   console.log("hourlyData : " + JSON.stringify(this.hourlyData));
+
+          this.dataInitialised = true;
+          this.renderChart();
         }
-    }
+      })
+      .catch((err) => {
+        console.error("Error: " + err);
+      });
+  }
 
-    instantiateWeatherFetchWrapper(){
-        this.fetchWrapper = new FetchWrapper({ baseURL:this.integrationObject.Base_Url__c})
-        .setUrlParameters({
-            "q": this.city, 
-            "appId": this.integrationObject.Authentication_Token__c,
-            "units": "metric"
+  renderedCallback() {
+    if (this.chartJsInitialised) {
+      return;
+    }
+    console.log("entered render callback");
+
+    Promise.all([loadScript(this, chartminjs), loadScript(this, chartJsAdapterDate)])
+      .then(() => {
+        this.renderChart();
+        this.chartJsInitialised = true;
+      })
+      .catch((error) => {
+        console.error({
+          message: "Error occured on ChartJs",
+          error
         });
-        this.handleFetch();
+      });
+  }
+
+  renderChart() {
+    if (!this.dataInitialised || !this.chartJsInitialised) {
+      return;
     }
 
-    instantiateOneCallFetchWrapper(lon, lat){
-        this.fetchWrapper = new FetchWrapper({ baseURL:this.integrationObject.Base_Url__c})
-        .setUrlParameters({
-            "lon": lon, 
-            "lat": lat, 
-            "appId": this.integrationObject.Authentication_Token__c,
-            "exclude": "minutely",
-            "units": "metric"
-        });
-        this.endpoint = "onecall";
-        this.handleFetch();
-    }
+    console.log("this.dataInitialised: " + this.dataInitialised);
+    console.log("this.chartJsInitialised: " + this.chartJsInitialised);
 
-    handleFetch(){
-        this.fetchWrapper.get(this.endpoint, {})
-        .then(data => {
-            if(this.endpoint === "weather"){
-                data.weather = data.weather[0];
-                data.weather.icon = this.weatherIconPath + this.getWeatherIcon(data.weather.main);
-                data.dt = String(data.dt).padEnd(13,"0");
-                this.weatherData = data;
-                console.log("Weather Data: " + JSON.stringify(data));
-                this.instantiateOneCallFetchWrapper(data.coord.lon, data.coord.lat);
-            } else {
-                data.daily.splice(0,1);
-                data.daily.splice(5);
-                data.daily.forEach(day => {
-                    day.weather = day.weather[0];
-                    day.weather.icon = this.weatherIconPath + this.getWeatherIcon(day.weather.main);
-                    day.dt = String(day.dt).padEnd(13,"0");
-                    day.sunrise = String(day.sunrise).padEnd(13,"0");
-                    day.sunset = String(day.sunset).padEnd(13,"0");
-                    day.moonrise = String(day.moonrise).padEnd(13,"0");
-                    day.moonset = String(day.moonset).padEnd(13,"0");
-                });
-                data.hourly = data.hourly.filter(hr => new Date(Number.parseInt(String(hr.dt).padEnd(13,"0"), 10)).getHours() % 3 == 0);
-                data.hourly.forEach(hr => {
-                    hr.weather = hr.weather[0];
-                    hr.weather.icon = this.weatherIconPath + this.getWeatherIcon(hr.weather.main);
-                    hr.dt = String(hr.dt).padEnd(13,"0");
-                    hr.sunrise = String(hr.sunrise).padEnd(13,"0");
-                    hr.sunset = String(hr.sunset).padEnd(13,"0");
-                    hr.moonrise = String(hr.moonrise).padEnd(13,"0");
-                    hr.moonset = String(hr.moonset).padEnd(13,"0");
-                });
-                
-                this.dailyData = data.daily;
+    // const labels = ["January", "February", "March", "April", "May", "June"];
 
-                console.log("dailyData : " + JSON.stringify(data.daily));
+    // const config = {
+    //   type: "line",
+    //   data: {
+    //     labels: labels,
+    //     datasets: [
+    //       {
+    //         label: "My First dataset",
+    //         backgroundColor: "rgb(255, 99, 132)",
+    //         borderColor: "rgb(255, 99, 132)",
+    //         data: [0, 10, 5, 2, 20, 30, 45]
+    //       }
+    //     ]
+    //   },
+    //   options: {}
+    // };
+
+    const config = {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            tension: 0.4,
+            fill: false,
+            label: "Line Dataset 1",
+            data: this.hourlyData,
+            backgroundColor: ["#80aaff"],
+            borderColor: ["blue"],
+            pointBackgroundColor: "#80aaff",
+            pointBorderColor: "blue"
+          }
+        ]
+      },
+      options: {
+        tension: 0.4,
+        title: {
+          display: true,
+          text: "Sand Samples Against Comm Weight %."
+        },
+        scales: {
+          xAxes: {
+            type: "time",
+            time: {
+              //   unit: "hour",
+              //   displayFormats: {
+              //     hour: "HH:mm"
+              //   }
             }
-        })
-        .catch(err => {
-            console.error("Error: " + err);
-        });
-    }
+          },
+          yAxes: {
+            type: "linear",
+            ticks: {
+              autoSkip: true,
+              suggestedMin: 0,
+              suggestedMax: 100,
+              stepSize: 1
+            }
+          }
+        }
+      }
+    };
 
+    const ctx = this.template.querySelector("canvas.linechart").getContext("2d");
+    this.chart = new window.Chart(ctx, config);
+    this.chart.canvas.parentNode.style.height = "100%";
+    this.chart.canvas.parentNode.style.width = "100%";
+  }
 }
